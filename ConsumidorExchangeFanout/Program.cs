46042a699902282256db7b3ced586f9072fc994c
augusto <augustosaaude@gmail.com>
2024-05-19 15:54:34 -0300

    feat: add exchange example of fanout

---
+++ ConsumidorExchangeFanout/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ConsumidorExchangeFanout;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+
+        using (var connection = factory.CreateConnection())
+        {
+            var channel = connection.CreateModel();
+
+            //var queueName = "logs-exchange-fanout";
+            //var queueName = "order-exchange-fanout";
+            var queueName = "finance-orders_exchange-fanout";
+
+            channel.QueueDeclare(
+                queue: queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            BuildAndRunWorker(channel, "Worker A", queueName);
+            BuildAndRunWorker(channel, "Worker B", queueName);
+
+            Console.ReadLine();
+        }
+    }
+
+    public static void BuildAndRunWorker(IModel channel, string workerName, string queueName)
+    {
+        channel.BasicQos(0, 3, false); 
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, ea) =>
+        {
+            try
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"{channel.ChannelNumber} - {workerName} [x] Received {message}");
+
+                channel.BasicAck(ea.DeliveryTag, false);
+            }
+            catch (Exception ex)
+            {
+                channel.BasicNack(ea.DeliveryTag, false, true);
+            }
+        };
+
+        channel.BasicConsume(
+            queue: queueName,
+            autoAck: false,
+            consumer: consumer
+        );
+    }
+}
---
+++ ProdutorExchangeFanout/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace ProdutorExchangeFanout;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        var manualResetEvent = new ManualResetEvent(false);
+
+        manualResetEvent.Reset();
+
+        using (var connection = factory.CreateConnection())
+        {
+            var channel1 = SetupChannel(connection);
+
+            BuildPublishers(channel1, "Produtor A", manualResetEvent);
+
+            manualResetEvent.WaitOne();
+        }
+    }
+
+    public static IModel SetupChannel(IConnection connection)
+    {
+        var channel = connection.CreateModel();
+
+        var logsQueue = "logs-exchange-fanout";
+        var orderQueue = "order-exchange-fanout";
+        var financeOrdersQueues = "finance-orders_exchange-fanout";
+
+        channel.QueueDeclare(
+            queue: orderQueue,
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        channel.QueueDeclare(
+            queue: logsQueue,
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        channel.QueueDeclare(
+            queue: financeOrdersQueues,
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        channel.ExchangeDeclare("order", "fanout");
+
+        channel.QueueBind(orderQueue, "order", string.Empty);
+        channel.QueueBind(logsQueue, "order", string.Empty);
+        channel.QueueBind(financeOrdersQueues, "order", string.Empty);
+
+        return channel;
+    }
+
+    public static void BuildPublishers(IModel channel, string publisherName, ManualResetEvent manualResetEvent)
+    {
+        Task.Run(() =>
+        {
+            int count = 0;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Pressione qualquer tecla para produzir 100 msg");
+                    Console.ReadLine();
+
+                    for (var index = 0; index < 100; index++)
+                    {
+                        string message = $"OrderNumber: {count++} from {publisherName}";
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish("order", string.Empty, null, body);
+                        Console.WriteLine($"{publisherName} - [x] Sent {count}", message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+
+                    manualResetEvent.Set();
+                }
+            }
+        });
+    }
+}
